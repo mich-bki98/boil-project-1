a9b1d7eb1c1b30b2bc62e1e0ef539911bed286f6
mbmich <mb.mich98@gmail.com>
2020-11-29 18:10:06 +0100

    Fin

--- src/model/InputModel.java
+++ src/model/InputModel.java
@@ -1,9 +1,9 @@
 package model;
 
 public class InputModel {
-    private double[] supply = new double[3];
-    private double[] demand = new double[3];
-    double[] profitArr = new double[9];
+    private double[] supply;
+    private double[] demand;
+    double[] profitArr;
 
     public InputModel(double[] supply, double[] demand, double[] profitArr) {
         this.supply = supply;
@@ -15,23 +15,12 @@ public class InputModel {
         return supply;
     }
 
-    public void setSupply(double[] supply) {
-        this.supply = supply;
-    }
-
     public double[] getDemand() {
         return demand;
-    }
-
-    public void setDemand(double[] demand) {
-        this.demand = demand;
     }
 
     public double[] getProfitArr() {
         return profitArr;
     }
 
-    public void setProfitArr(double[] profitArr) {
-        this.profitArr = profitArr;
-    }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import model.InputModel;
-import org.apache.commons.math3.linear.*;
 import org.apache.commons.math3.optimization.GoalType;
 import org.apache.commons.math3.optimization.PointValuePair;
 import org.apache.commons.math3.optimization.linear.LinearConstraint;
@@ -29,7 +28,8 @@ public class Controller {
     @FXML
     void initialize() {
         button.setOnAction(this::calculate);
-        this.inputModel = new InputModel(new double[]{20d, 40d, 10d}, new double[]{15d, 25d, 30d},
+        this.inputModel = new InputModel(new double[]{20d, 40d, 10d},
+                new double[]{15d, 20d, 35d},
                 new double[]{7d, 2d, 6d,
                         2d, -2d, 3d,
                         5d, 2d, 0d});
@@ -41,14 +41,15 @@ public class Controller {
 
         LinearObjectiveFunction f = new LinearObjectiveFunction(inputModel.getProfitArr(), 0);
 
-        Collection<LinearConstraint> constraints = new ArrayList<LinearConstraint>();
-        constraints.add(new LinearConstraint(new double[]{1, 1, 1, 0, 0, 0, 0, 0, 0}, Relationship.EQ, inputModel.getSupply()[0]));
-        constraints.add(new LinearConstraint(new double[]{0, 0, 0, 1, 1, 1, 0, 0, 0}, Relationship.LEQ, inputModel.getSupply()[1]));
-        constraints.add(new LinearConstraint(new double[]{0, 0, 0, 0, 0, 0, 1, 1, 1}, Relationship.EQ, inputModel.getSupply()[2]));
+        Collection<LinearConstraint> constraints = new ArrayList<>();
 
-        constraints.add(new LinearConstraint(new double[]{1, 0, 0, 1, 0, 0, 1, 0, 0}, Relationship.GEQ, inputModel.getDemand()[0]));
+        constraints.add(new LinearConstraint(new double[]{1, 1, 1, 0, 0, 0, 0, 0, 0}, Relationship.LEQ, inputModel.getSupply()[0]));
+        constraints.add(new LinearConstraint(new double[]{0, 0, 0, 1, 1, 1, 0, 0, 0}, Relationship.LEQ, inputModel.getSupply()[1]));
+        constraints.add(new LinearConstraint(new double[]{0, 0, 0, 0, 0, 0, 1, 1, 1}, Relationship.LEQ, inputModel.getSupply()[2]));
+
+        constraints.add(new LinearConstraint(new double[]{1, 0, 0, 1, 0, 0, 1, 0, 0}, Relationship.LEQ, inputModel.getDemand()[0]));
         constraints.add(new LinearConstraint(new double[]{0, 1, 0, 0, 1, 0, 0, 1, 0}, Relationship.GEQ, inputModel.getDemand()[1]));
-        constraints.add(new LinearConstraint(new double[]{0, 0, 1, 0, 0, 1, 0, 0, 1}, Relationship.GEQ, inputModel.getDemand()[2]));
+        constraints.add(new LinearConstraint(new double[]{0, 0, 1, 0, 0, 1, 0, 0, 1}, Relationship.LEQ, inputModel.getDemand()[2]));
 
 
         try {
