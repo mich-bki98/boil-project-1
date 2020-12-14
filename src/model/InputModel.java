a7e4524904363abe1aab53e61339e3b223dde0ee
mbmich <mb.mich98@gmail.com>
2020-12-15 00:38:36 +0100

    Fin version

--- src/model/InputModel.java
+++ src/model/InputModel.java
@@ -4,11 +4,28 @@ public class InputModel {
     private double[] supply;
     private double[] demand;
     double[] profitArr;
+    double[] sells;
+    double[] buys;
+    double[] routeArr;
 
-    public InputModel(double[] supply, double[] demand, double[] profitArr) {
+    public InputModel(double[] supply, double[] demand, double[] sells, double[] buys,double[] routeArr) {
         this.supply = supply;
         this.demand = demand;
-        this.profitArr = profitArr;
+        this.sells = sells;
+        this.buys = buys;
+        this.routeArr = routeArr;
+
+        this.profitArr = new double[]{
+                sells[0] - routeArr[0] - buys[0],
+                sells[1] - routeArr[1] - buys[0],
+                sells[2] - routeArr[2] - buys[0],
+                sells[0] - routeArr[3] - buys[1],
+                sells[1] - routeArr[4] - buys[1],
+                sells[2] - routeArr[5] - buys[1],
+                sells[0] - routeArr[6] - buys[2],
+                sells[1] - routeArr[7] - buys[2],
+                sells[2] - routeArr[8] - buys[2]
+        };
     }
 
     public double[] getSupply() {
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import model.InputModel;
 import org.apache.commons.math3.optimization.GoalType;
 import org.apache.commons.math3.optimization.PointValuePair;
@@ -12,6 +13,8 @@ import org.apache.commons.math3.optimization.linear.LinearObjectiveFunction;
 import org.apache.commons.math3.optimization.linear.Relationship;
 import org.apache.commons.math3.optimization.linear.SimplexSolver;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -23,21 +26,104 @@ public class Controller {
     @FXML
     private Label wynik;
 
-    private InputModel inputModel;
+    //Demands
+    @FXML
+    private TextField o1demand;
+    @FXML
+    private TextField o2demand;
+    @FXML
+    private TextField o3demand;
 
+    //Supplies
+    @FXML
+    private TextField d1supp;
+    @FXML
+    private TextField d2supp;
+    @FXML
+    private TextField d3supp;
+
+    //Sell Prices
+    @FXML
+    private TextField sello1;
+    @FXML
+    private TextField sello2;
+    @FXML
+    private TextField sello3;
+
+    //Buy Prices
+    @FXML
+    private TextField buyd1;
+    @FXML
+    private TextField buyd2;
+    @FXML
+    private TextField buyd3;
+
+    //route arr
+    @FXML
+    private TextField d1o1;
+    @FXML
+    private TextField d1o2;
+    @FXML
+    private TextField d1o3;
+    @FXML
+    private TextField d2o1;
+    @FXML
+    private TextField d2o2;
+    @FXML
+    private TextField d2o3;
+    @FXML
+    private TextField d3o1;
+    @FXML
+    private TextField d3o2;
+    @FXML
+    private TextField d3o3;
+
+    private InputModel inputModel;
+    private double[] demandArr;
+    private double[] supplyArr;
+    private double[] sellPrices;
+    private double[] buyPrices;
+    private double[] routeArr;
+
+
+    //230.0
     @FXML
     void initialize() {
-        button.setOnAction(this::calculate);
-        this.inputModel = new InputModel(new double[]{20d, 40d, 10d},
-                new double[]{15d, 20d, 35d},
-                new double[]{7d, 2d, 6d,
-                        2d, -2d, 3d,
-                        5d, 2d, 0d});
+        demandArr = new double[]{Double.parseDouble(o1demand.getText()),
+                Double.parseDouble(o2demand.getText()),
+                Double.parseDouble(o3demand.getText())};
 
+        supplyArr = new double[]{Double.parseDouble(d1supp.getText()),
+                Double.parseDouble(d2supp.getText()),
+                Double.parseDouble(d3supp.getText())};
+
+        sellPrices = new double[]{Double.parseDouble(sello1.getText()),
+                Double.parseDouble(sello2.getText()),
+                Double.parseDouble(sello3.getText())};
+
+        buyPrices = new double[]{Double.parseDouble(buyd1.getText()),
+                Double.parseDouble(buyd2.getText()),
+                Double.parseDouble(buyd3.getText())};
+
+        routeArr = new double[]{Double.parseDouble(d1o1.getText()),
+                Double.parseDouble(d1o2.getText()),
+                Double.parseDouble(d1o3.getText()),
+                Double.parseDouble(d2o1.getText()),
+                Double.parseDouble(d2o2.getText()),
+                Double.parseDouble(d2o3.getText()),
+                Double.parseDouble(d3o1.getText()),
+                Double.parseDouble(d3o2.getText()),
+                Double.parseDouble(d3o3.getText())};
+
+        button.setOnAction(this::calculate);
     }
+
 
     @FXML
     public void calculate(ActionEvent event) {
+
+        inputModel = new InputModel(supplyArr,demandArr,sellPrices,buyPrices,routeArr);
+
 
         LinearObjectiveFunction f = new LinearObjectiveFunction(inputModel.getProfitArr(), 0);
 
@@ -60,5 +146,26 @@ public class Controller {
             String[] str = ex.toString().split(":");
             wynik.setText(str[1]);
         }
+
+        try {
+            FileWriter fw = new FileWriter("output.txt",false);
+            fw.write("Tabela jednostkowa: \n");
+            int id=0;
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    fw.write(Double.toString(inputModel.getProfitArr()[id]));
+                    fw.write(" ");
+                    id++;
+                }
+                fw.write("\n");
+            }
+            fw.write("Zysk całkowity: \n");
+            fw.write(wynik.getText());
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
